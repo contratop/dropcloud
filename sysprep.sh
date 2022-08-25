a9ff15dd8a5d109625ee9071d9186f3493965bd1
ContratopDev <contratop@molecule.net>
2022-08-25 23:56:11 +0200

    Auto-Commit

---
+++ sysprep.sh
@@ -0,0 +1,20 @@
+##Todo en termux, cada linea es un comando a ejecutar:
+
+
+apt update
+
+apt upgrade
+
+apt install proot
+
+apt install proot-distro
+
+proot-distro install debian
+
+## (Vamos a editar el .bashrc)
+
+## en la primera linea ponemos:
+echo "proot-distro login debian"  >> .bashrc
+
+
+
