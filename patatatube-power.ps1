aa7cd80c8ac2cb3301e1ba5fdc1fa68e59a43564
ContratopDev <contratop@molecule.net>
2022-08-27 05:51:11 +0200

    Auto-Commit

--- patatatube-power.ps1
+++ patatatube-power.ps1
@@ -268,7 +268,7 @@ elseif($menu -eq "more"){
         Write-Warning "Debug Updater"
         write-host "Only for developers" -ForegroundColor Yellow
         Remove-Item patatatube-power.ps1
-        Invoke-WebRequest -uri https://raw.githubusercontent.com/contratop/dropcloud/main/patatatube-power.ps1 -OutFile patatatube-power.ps1
+        Invoke-WebRequest -uri https://raw.githubusercontent.com/pokeinalover/PatataTube/main/patatatube-power.ps1 -OutFile patatatube-power.ps1
         write-host "Finished!" -ForegroundColor Green
         exit
     }
